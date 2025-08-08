8ae0b65c86d033ba1c18c50c449264800e5427d0
DASHAID <103207086+DASHAID@users.noreply.github.com>
2025-08-08 21:40:23 +0700

    Update index.php

--- ex2/feedback/index.php
+++ ex2/feedback/index.php
@@ -9,7 +9,7 @@ $APPLICATION->IncludeComponent("bitrix:main.feedback",
 [
 	"EMAIL_TO" => "admin@example.com",
 	"EVENT_MESSAGE_ID" => ["FEEDBACK_FORM"],
-	"OK_TEXT" => "Thank you, message send",
+	"OK_TEXT" => "Сообщение отправлено!",
 	"REQUIRED_FIELDS" => ["NAME", "EMAIL", "MESSAGE"],
 	"USE_CAPTCHA" => "Y"
 ]
