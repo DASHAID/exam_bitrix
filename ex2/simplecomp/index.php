bcc33cfc096d3993df0063d68a6bcbb34f5869b2
Chernova <pro100turistka@yandex.ru>
2025-08-10 13:51:26 +0000

    new infoblock

--- ex2/simplecomp/index.php
+++ ex2/simplecomp/index.php
@@ -7,9 +7,9 @@ $APPLICATION->IncludeComponent(
 	".default",
 	array(
 		"CATALOG_IBLOCK_ID" =>"2",
-		"CLASSIFIER_IBLOCK_ID" =>"3",
+		"CLASSIFIER_IBLOCK_ID" =>"14",
 		"LINK_PROPERTY_CODE"=>"FIRMA",
-		"DETAIL_URL" => "/catalog/#ELEMENT_CODE#",
+		"DETAIL_URL" => "/catalog/detail.php?ELEMENT_ID=#ELEMENT_ID##",
 		"CACHE_TYPE" =>"A",
 		"CACHE_TIME" =>3600,
 	),
