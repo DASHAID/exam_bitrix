c0171bb696528bf99454f21a7ee9d974c69b2e39
Chernova <pro100turistka@yandex.ru>
2025-08-10 04:07:22 +0000

    no ok

--- ex2/simplecomp/index.php
+++ ex2/simplecomp/index.php
@@ -3,16 +3,17 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Простой компонент");
 
 $APPLICATION->IncludeComponent(
-	"simplecomp.exam",
-	"",
-	[
+	"simplecomp:exam",
+	".default",
+	array(
 		"CATALOG_IBLOCK_ID" =>"2",
 		"CLASSIFIER_IBLOCK_ID" =>"3",
 		"LINK_PROPERTY_CODE"=>"FIRMA",
-		"DETAIL_URL" => "/catalog/detail.php?ELEMENT_ID=#ELEMENT_ID#",
+		"DETAIL_URL" => "/catalog/#ELEMENT_CODE#",
 		"CACHE_TYPE" =>"A",
 		"CACHE_TIME" =>3600,
-	]
+	),
+	false
 );
 
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
